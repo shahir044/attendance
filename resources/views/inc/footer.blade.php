0a950ad38724b47394b9bc9939f0a60580ea0d47
nazrul42 <bgit42@gmail.com>
2021-04-25 13:04:59 +0600

    footer updated

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,6 +1,6 @@
 
 
-<footer class="new_footer_area bg_color">
+{{-- <footer class="new_footer_area bg_color">
     
     <div class="footer_bottom">
         
@@ -20,4 +20,16 @@
         </div>
         <div ><a href="#"><img src="{{ asset('images/biman.png') }}" class="img-fluid" alt="logo"></a></div> 
     </div>
+</footer> --}}
+
+
+<footer class="mt-5 float-right" style="display: block; width: 100%; margin-bottom:0px;">
+    
+    <p class="float-right" style="margin-bottom:-10%; margin-right: 5%">
+        &#169; IT-Division, Biman Bangladesh Airlines
+      </p>
+    <img
+    src="{{ asset('images/wav1.png') }}"
+      style="display: block; width: 100%;"
+    />
 </footer>
