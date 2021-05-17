ac2a58ce817a14009c081129ca4619049de4ece0
SHAHIR <bgit044@gmail.com>
2021-05-17 09:48:31 +0600

    month updated

--- resources/views/pages/month.blade.php
+++ resources/views/pages/month.blade.php
@@ -4,7 +4,7 @@
     $host = "localhost";
     $username = "root";
     $password = "";
-    $database = "attendance";
+    $database = "attendence";
 
     $conn = mysqli_connect($host, $username, $password, $database);
 ?>
